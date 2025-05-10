1452755d9c0a0224d3196f009f3bdcc2aedb96e4
Abishek Narayan V <127777179+Abishek-49@users.noreply.github.com>
2025-05-10 13:22:43 +0530

    Update contactForm.js
    Updated Google Apps Script URL because previous URL not working

--- contactForm.js
+++ contactForm.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxOksF9_Ul6fVbR9rMnahotuZuvrZEtOGW8z8PtPXo2-qraFBtF8X2Y3b802f7QkiIbBg/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycby3Th8CHZGV7HF-PIghtalNWbH8qh1-nkz-Oh7Vkt9KBnrQCNvdrWvbDn3bAjLwDbkbKQ/exec'
 const form = document.forms['submit-to-google-sheet'];
 const loadingIndicator = document.getElementById('loading');
 
